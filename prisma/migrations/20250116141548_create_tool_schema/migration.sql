50bb9f0b88d6d602fbc9392c76b692e34dfbcc6b
Joaopsguimaraes <psjoaovictor@gmail.com>
2025-01-16 11:16:10 -0300

    fix: prisma conection and migration

--- prisma/migrations/20250116141548_create_tool_schema/migration.sql
+++ prisma/migrations/20250116141548_create_tool_schema/migration.sql
@@ -1,17 +1,21 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
-    "email" TEXT NOT NULL
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Tool" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "url" TEXT NOT NULL,
     "description" TEXT NOT NULL,
-    "tags" TEXT NOT NULL
+    "tags" TEXT NOT NULL,
+
+    CONSTRAINT "Tool_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
